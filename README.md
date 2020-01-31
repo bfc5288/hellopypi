1d761eba891e5a104d43f3f8bb62b184ca882aa3
bfc5288 <bfc5288@psu.edu>
2020-01-31 13:34:37 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a **test** readme markdown for a `PyPi` helloworld.
---
+++ setup.py
@@ -0,0 +1,18 @@
+import os
+from setuptools import setup
+
+# read in README                                                                                     
+this_dir = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_dir, 'README.md'), 'rb') as f:
+    long_description = f.read().decode().strip()
+
+setup(
+    name='hellopypi_bfc',
+    packages=['hellopypi_bfc'],
+    description='Hello world for pypi',
+    version='0.0.1',
+    #url='http://github.com/example/hellopypi',
+    author='bfc5288',
+    keywords=['pip','helloworld']
+    )
+
