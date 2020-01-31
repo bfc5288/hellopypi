fa0265208f265f323814cce389ce4387b5ac0818
bfc5288 <bfc5288@psu.edu>
2020-01-31 16:04:00 -0500

    changed version checking

--- hellopypi_bfc/__init__.py
+++ hellopypi_bfc/__init__.py
@@ -1,4 +1,9 @@
 __import__("pkg_resources").declare_namespace(__name__)
 
+try:
+    from ._version import version as __version__
+except ImportError:
+    pass
+
 def hello_world():
     print('Greetings starshine, the earth says hello')
