9697ae41df30d408c6289468a4c2f7d71e39fea3
bfc5288 <bfc5288@psu.edu>
2020-04-17 15:57:34 -0400

    Add uncovered file to reduce CodeCov coverage

---
+++ hellopypi_bfc/client/empty_no_test.py
@@ -0,0 +1,60 @@
+
+def sample_func1():
+    '''I'm just here to offer empty lines that aren't in pytest, to change the CodeCov report.
+
+    '''
+
+    cool_lambda = lambda x, y, z: print(f'this is x: {x}, y: {y}, z: {z}. Cool')
+
+    cool_lambda('what', 'is', 'up')
+
+    a = 1
+    few = 2
+    more = 3
+    empty = 4
+    lines = 5
+    without = 6
+    meaningful = 7
+    content = 8
+
+    return 0
+
+def sample_func2():
+    '''I'm just here to offer empty lines that aren't in pytest, to change the CodeCov report.
+
+    '''
+
+    cool_lambda = lambda x, y, z: print(f'this is x: {x}, y: {y}, z: {z}. Cool')
+
+    cool_lambda('what', 'is', 'up')
+
+    a = 1
+    few = 2
+    more = 3
+    empty = 4
+    lines = 5
+    without = 6
+    meaningful = 7
+    content = 8
+
+    return 0
+
+def sample_func3():
+    '''I'm just here to offer empty lines that aren't in pytest, to change the CodeCov report.
+
+    '''
+
+    cool_lambda = lambda x, y, z: print(f'this is x: {x}, y: {y}, z: {z}. Cool')
+
+    cool_lambda('what', 'is', 'up')
+
+    a = 1
+    few = 2
+    more = 3
+    empty = 4
+    lines = 5
+    without = 6
+    meaningful = 7
+    content = 8
+
+    return 0
